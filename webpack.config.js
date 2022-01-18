b1454d52fae135606fae29ceea2f23ae1e7084cb
Edward <v-tazho@hotmail.com>
2022-01-18 15:03:54 +0800

    enable docker

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ const CustomFunctionsMetadataPlugin = require("custom-functions-metadata-plugin"
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const glob = require("glob");
 const urlDev = "https://localhost:3000/";
-const urlProd = "https://www.contoso.com/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
+const urlProd = "https://www.excel.meekou.cn/"; // CHANGE THIS TO YOUR PRODUCTION DEPLOYMENT LOCATION
 
 /* global require, module, process, __dirname */
 
